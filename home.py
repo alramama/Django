c2b411c9f1fa23d1ecb4c154df5e7c35d2d48ebf
alramama <74817864+alramama@users.noreply.github.com>
2022-07-06 12:16:24 +0300

    Update home.py

--- home.py
+++ home.py
@@ -3,6 +3,22 @@ from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [ path('', views.home, name="home"),]
 
+class Order(models.Model):
+	STATUS = (
+			('Pending', 'Pending'),
+			('Out for delivery', 'Out for delivery'),
+			('Delivered', 'Delivered'),
+			)
+
+	customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	status = models.CharField(max_length=200, null=True, choices=STATUS)
+	note = models.CharField(max_length=1000, null=True)
+
+	def __str__(self):
+		return self.product.name
+
 def home(request):
 	orders = Order.objects.all()
 	customers = Customer.objects.all()
